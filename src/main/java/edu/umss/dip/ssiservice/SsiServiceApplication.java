8014caa0bd09d9c155623a51567861eb04c4a1c1
Rudolf Felipez <rudolf.flodur@gmail.com>
2019-02-20 06:05:13 -0400

    primer commit

---
+++ src/main/java/edu/umss/dip/ssiservice/SsiServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.umss.dip.ssiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiServiceApplication.class, args);
+    }
+
+}
