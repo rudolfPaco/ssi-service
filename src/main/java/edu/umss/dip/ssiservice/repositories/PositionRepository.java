6a96a1d23016b1381b1d97f4ccb247e95882da15
rudol <rudolf.flodur@gmail.com>
2019-02-23 02:11:29 -0400

    repositorios

---
+++ src/main/java/edu/umss/dip/ssiservice/repositories/PositionRepository.java
@@ -0,0 +1,7 @@
+package edu.umss.dip.ssiservice.repositories;
+
+import edu.umss.dip.ssiservice.model.Position;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PositionRepository extends CrudRepository<Position, Long> {
+}
